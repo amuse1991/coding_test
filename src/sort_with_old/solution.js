03e754352b86ba99d5e78fcf1f6a820eb3bea922
신윤호 <amuse1991@gmail.com>
2020-07-28 10:41:16 +0900

    [10814:나이순정렬] 풀이 추가

---
+++ src/sort_with_old/solution.js
@@ -0,0 +1,23 @@
+export default function(inputs) {
+  inputs = inputs.slice(1);
+  const ageMap = inputs.reduce((resMap, member) => {
+    const [age, name] = member.split(" ");
+    if (!resMap.has(age)) {
+      resMap.set(age, []);
+    }
+    resMap.get(age).push(name);
+    return resMap;
+  }, new Map());
+
+  const ageEntries = [...ageMap.entries()].sort();
+  const results = ageEntries.reduce((res, item) => {
+    const [age, ...members] = item;
+    return [
+      ...res,
+      ...Array.from(...members, member => {
+        return `${age} ${member}`;
+      })
+    ];
+  }, []);
+  return results;
+}
---
+++ src/sort_with_old/solution.spec.js
@@ -0,0 +1,7 @@
+import solution from "./solution";
+
+test("sort_with_old", () => {
+  const input = ["3", "21 Junkyu", "21 Dohyun", "20 Sunyoung"];
+  const sol = solution(input);
+  expect(sol).toEqual(["20 Sunyoung", "21 Junkyu", "21 Dohyun"]);
+});
